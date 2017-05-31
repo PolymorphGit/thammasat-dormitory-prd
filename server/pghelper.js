d20fd2d341d23ed00cc18734b7fb74df21698488
PolymorphGit <info.polymorph@gmail.com>
2017-05-31 14:05:21 +0700

    Update pghelper.js

--- server/pghelper.js
+++ server/pghelper.js
@@ -2,7 +2,8 @@
 
 var pg = require('pg'),
     //databaseURL = process.env.DATABASE_URL || 'postgres://ehuotalmpdqjvs:da48536ca63cdb9f209d7e00695d5e261441f7313b611d670bf104bbb1d24a5a@ec2-54-243-214-198.compute-1.amazonaws.com:5432/df3pgi81qfmoc7';
-	databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
+    //databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
+	databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/d89pl2t2eqpara';
 /*
 if (process.env.DATABASE_URL !== undefined) 
 {
