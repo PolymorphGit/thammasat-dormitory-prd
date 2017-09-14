8136ccab3c1c38f0302e7599a0e54976588e2d50
PolymorphGit <info.polymorph@gmail.com>
2017-09-14 10:45:22 +0700

    Update task.js

--- server/task.js
+++ server/task.js
@@ -53,16 +53,8 @@ exports.getFeed = function(req, res, next) {
 								{
 									results2[i].status = 'Completed';
 								}
-								//results2[i].detail = results2[i].detail.replace('Morning', 'เช้า');
-								//results2[i].detail = results2[i].detail.replace('Afternoon', 'บ่าย');
-								if(results2[i].detail.substr(results2[i].detail.lenght - 7, results2[i].detail.lenght) == 'Morning')
-								{
-									results2[i].detail = results2[i].detail.substr(0, results2[i].detail.lenght - 7) + 'เช้า';
-								}
-								else
-								{
-									results2[i].detail = results2[i].detail.substr(0, results2[i].detail.lenght - 9) + 'บ่าย';
-								}
+								results2[i].detail = results2[i].detail.replace(/Morning/g, 'เช้า');
+								results2[i].detail = results2[i].detail.replace(/Afternoon/g, 'บ่าย');
 							}
 							res.json(results2)
 						})
