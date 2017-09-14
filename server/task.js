c78e01a369ce8722de9337cd7e017a3bc1f5a111
PolymorphGit <info.polymorph@gmail.com>
2017-09-14 10:36:13 +0700

    Update task.js

--- server/task.js
+++ server/task.js
@@ -55,7 +55,7 @@ exports.getFeed = function(req, res, next) {
 								}
 								//results2[i].detail = results2[i].detail.replace('Morning', 'เช้า');
 								//results2[i].detail = results2[i].detail.replace('Afternoon', 'บ่าย');
-								if(results2[i].detail.includes('Morning'))
+								if(results2[i].detail.search('Morning') > -1)
 								{
 									results2[i].detail = results2[i].detail.substr(0, results2[i].detail.lenght - 7) + 'เช้า';
 								}
