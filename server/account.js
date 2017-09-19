3b3dbd3240dff7d1a67434588218a55360725b61
PolymorphGit <info.polymorph@gmail.com>
2017-09-20 01:28:00 +0700

    Update account.js

--- server/account.js
+++ server/account.js
@@ -352,7 +352,7 @@ exports.getmobileid = function(req, res, next) {
 					results2.on('end', function() {
 						try
 						{
-							str2 = str2.substr(1, str2.length - 1);
+							str2 = str2.substr(1, str2.length - 2);
 							console.log(str2);	
 							var obj2 = JSON.parse(str2);
 							console.log('Mobile Id : ' + obj2.identities[0].user_id);
