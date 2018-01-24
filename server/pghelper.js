7aa0bd4371a6d675cce6d7af291f8cf35e7b16aa
PolymorphGit <info.polymorph@gmail.com>
2018-01-24 13:30:45 +0700

    Update pghelper.js

--- server/pghelper.js
+++ server/pghelper.js
@@ -1,11 +1,11 @@
 "use strict";
-/*
+
 var pg = require('pg'),
     //databaseURL = process.env.DATABASE_URL || 'postgres://ehuotalmpdqjvs:da48536ca63cdb9f209d7e00695d5e261441f7313b611d670bf104bbb1d24a5a@ec2-54-243-214-198.compute-1.amazonaws.com:5432/df3pgi81qfmoc7';
     //databaseURL = process.env.DATABASE_URL || 'postgres://localhost:5432/df3pgi81qfmoc7';
     //console.log(process.env.DATABASE_URL);
-	databaseURL = process.env.DATABASE_URL || 'postgres://u8imgdodm3jkq2:pccc07f14d7068e19ddbf5fd48ccdc226070cff71d26055390fa199c454a79c2d@ec2-52-86-233-50.compute-1.amazonaws.com:5432/d89pl2t2eqpara';
-	console.log(databaseURL);
+    databaseURL = process.env.DATABASE_URL || 'postgres://u8imgdodm3jkq2:pccc07f14d7068e19ddbf5fd48ccdc226070cff71d26055390fa199c454a79c2d@ec2-52-86-233-50.compute-1.amazonaws.com:5432/d89pl2t2eqpara';
+    console.log(databaseURL);
 
 const Pool = require('pg-pool');
 
@@ -19,10 +19,6 @@ const config = {
   max: 10,
   idleTimeoutMillis: 30000,
 };
-*/
-var pg = require('pg'),
-    Q = require('q'),
-    databaseURL = process.env.DATABASE_URL || 'postgres://u8imgdodm3jkq2:pccc07f14d7068e19ddbf5fd48ccdc226070cff71d26055390fa199c454a79c2d@ec2-52-86-233-50.compute-1.amazonaws.com:5432/d89pl2t2eqpara';
 
 /*
 if (process.env.DATABASE_URL !== undefined) 
@@ -33,31 +29,6 @@ if (process.env.DATABASE_URL !== undefined)
 
 exports.select = function (sql) {
 	
-	var deferred = Q.defer();
-
-    	pg.connect(databaseURL, function (err, conn, done) {
-		if (err) return deferred.reject(err);
-		try {
-		    console.log('====Connected====');
-		    conn.query(sql, values, function (err, result) {
-			done();
-			console.log(sql);
-			if (err) {
-			    deferred.reject(err);
-			} else {
-			    //deferred.resolve(singleItem ? result.rows[0] : result.rows);
-			    deferred.resolve(result.rows);
-			}
-		    });
-		}
-		catch (e) {
-		    done();
-		    deferred.reject(e);
-		}
-	    });
-
-    return deferred.promise;
-	/*
 	return new Promise((resolve, reject) => {
 		
 		//var pool = pg.Pool()
@@ -66,7 +37,7 @@ exports.select = function (sql) {
 		//console.log('====Start Pool====');
 		//pg.connect(databaseURL, function (err, conn, done) {
 		pool.connect(databaseURL, (err, conn, done) => {
-			//console.log('====Connected====');
+			console.log('====Connected====');
 			if (err) reject(err);
 			try{
 				conn.query(sql, function (err, result) {
@@ -83,6 +54,6 @@ exports.select = function (sql) {
 		});
 		/*pool.on('error', function (err, client) {
 			console.error('idle client error', err.message, err.stack);
-		});
-	});*/
+		});*/
+	});
 };
