b17c6e9bc74fd372994c712b67d84169e8e35476
PolymorphGit <info.polymorph@gmail.com>
2018-01-24 13:42:35 +0700

    Update pghelper.js

--- server/pghelper.js
+++ server/pghelper.js
@@ -31,9 +31,9 @@ exports.select = function (sql) {
 	
 	return new Promise((resolve, reject) => {
 		
-		//var pool = pg.Pool()
+		var pool = pg.Pool()
 		//const pool = new Pool(config)
-		const pool = new Pool()
+		//const pool = new Pool()
 		//console.log('====Start Pool====');
 		//pg.connect(databaseURL, function (err, conn, done) {
 		pool.connect(databaseURL, (err, conn, done) => {
